10cf47ce26ac376974db81c3b7ec90c216eadbd0
gbswstudent <hohahahohaho09@gmail.com>
2025-04-25 10:23:07 +0900

    04/25

---
+++ ios_setting_clone/ios_setting_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_setting_cloneApp.swift
+//  ios_setting_clone
+//
+//  Created by jsy on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_setting_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios_setting_cloneTests/ios_setting_cloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_setting_cloneTests.swift
+//  ios_setting_cloneTests
+//
+//  Created by jsy on 4/24/25.
+//
+
+import Testing
+@testable import ios_setting_clone
+
+struct ios_setting_cloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
